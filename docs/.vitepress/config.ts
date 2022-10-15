a1b45f890ae02af3a23abf5c8175e0066224be83
yanquanfahei <1449826851@qq.com>
2022-10-15 11:58:36 +0800

    chore: init

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig, DefaultTheme } from 'vitepress'
 
 export default defineConfig({
+  lang: "zh-CN",
   title: '眼圈发黑',
   description: '前端知识体系学习博客',
-  base: 'blog'
+  lastUpdated: true,
+  base: '/blog',
+  themeConfig: {
+    siteTitle: 'My Blog',
+    logo: '/logo.png',
+    lastUpdatedText: '最后更新',
+    nav: getNavbar(),
+    sidebar: getSidebar(),
+  }
 })
+
+
+function getNavbar(): DefaultTheme.NavItem[] {
+  return [
+    {
+      text: 'Guide',
+      link: '/guide'
+    }
+  ]
+}
+
+function getSidebar(): DefaultTheme.Sidebar {
+  return [
+    {
+      text: 'Section Title A',
+      collapsible: true,
+      items: []
+    }
+  ]
+}
