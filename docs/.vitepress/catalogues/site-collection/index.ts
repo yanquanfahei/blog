060dba7257c965d522dffb60cd0637196d5b1165
yanquanfahei <1449826851@qq.com>
2023-02-10 10:48:24 +0800

    feat: add 引导

---
+++ docs/.vitepress/catalogues/site-collection/index.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'vitepress'
+export default {
+  text: '网站收集',
+  items: [
+    {
+      text: '引导',
+      link: '/site-collection/'
+    }
+  ]
+} as DefaultTheme.NavItem
