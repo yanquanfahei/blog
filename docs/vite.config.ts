d71d71c5ce50c616e99809b80856039fee1f788c
yanquanfahei <1449826851@qq.com>
2023-02-14 15:38:37 +0800

    feat: add unocss

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import { SearchPlugin } from 'vitepress-plugin-search'
+import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
   plugins: [
     SearchPlugin({
       // encode: false,
       tokenize: 'full'
-    })
+    }),
+    UnoCSS()
   ]
 })
