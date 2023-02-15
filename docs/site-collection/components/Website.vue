683445d0cf14e3301dfa8949fc20c779025aac31
yanquanfahei <lianfb@biyouxinli.com>
2023-02-15 12:54:13 +0800

    feat: add vue ecology

--- docs/site-collection/components/Website.vue
+++ docs/site-collection/components/Website.vue
@@ -1,21 +1,69 @@
 <script setup lang="ts">
 import WebsiteItem from './WebsiteItem.vue'
 
+interface IWebsiteItem {
+  title: string
+  desc: string
+  link: string
+  pic: string
+}
+
+interface IWebsiteMap {
+  title: string
+  list: Array<IWebsiteItem>
+}
+
 // vue 生态
-const vueEcology = {
+const vueEcology: IWebsiteMap = {
   title: 'Vue生态',
   list: [
     {
       title: 'Vue',
       desc: 'The Progressive JavaScript Framework',
       link: 'https://vuejs.org/',
-      pic: 'https://avatars.githubusercontent.com/u/6128107?s=200&v=4'
+      pic: 'https://router.vuejs.org/logo.png'
+    },
+    {
+      title: 'Vue Router',
+      desc: 'The official router for Vue.js',
+      link: 'https://router.vuejs.org/',
+      pic: 'https://router.vuejs.org/logo.png'
+    },
+    {
+      title: 'Nuxt',
+      desc: 'The Intuitive Vue Framework',
+      link: 'https://nuxtjs.org/',
+      pic: 'https://avatars.githubusercontent.com/u/23360933?s=200&v=4'
+    },
+    {
+      title: 'Pinia',
+      desc: 'The Vue Store that you will enjoy using',
+      link: 'https://pinia.vuejs.org/',
+      pic: 'https://pinia.vuejs.org/logo.svg'
+    },
+    {
+      title: 'Vuex',
+      desc: 'Vuex is a state management pattern + library for Vue.js',
+      link: 'https://vuex.vuejs.org/',
+      pic: 'https://router.vuejs.org/logo.png'
+    },
+    {
+      title: 'VitePress',
+      desc: "Simple, powerful, and performant. Meet the modern SSG framework you've always wanted.",
+      link: 'https://vitepress.vuejs.org/',
+      pic: 'https://router.vuejs.org/logo.png'
+    },
+    {
+      title: 'VuePress',
+      desc: 'Vue-powered Static Site Generator',
+      link: 'https://vuepress.vuejs.org/',
+      pic: 'https://vuepress.vuejs.org/hero.png'
     }
   ]
 }
 
 // 资源压缩
-const compressWebsite = {
+const compressWebsite: IWebsiteMap = {
   title: '资源压缩',
   list: [
     {
@@ -29,17 +77,11 @@ const compressWebsite = {
       desc: '可批量编辑图片 的所有工具',
       link: 'https://www.iloveimg.com/zh-cn',
       pic: 'https://www.iloveimg.com/img/iloveimg.svg'
-    },
-    {
-      title: 'iLoveIMG',
-      desc: '可批量编辑图片 的所有工具',
-      link: 'https://www.iloveimg.com/zh-cn',
-      pic: 'https://www.iloveimg.com/img/iloveimg.svg'
     }
   ]
 }
 
-const websiteSet = [vueEcology, compressWebsite]
+const websiteSet: Array<IWebsiteMap> = [vueEcology, compressWebsite]
 </script>
 <template>
   <div class="website-container">
